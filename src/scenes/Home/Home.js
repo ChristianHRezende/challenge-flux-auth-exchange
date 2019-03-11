f43dbca2a174ed7f796d5e9508d6c27c402b3da1
christian_srs_@hotmail.com <christian@fai-mg.br>
2019-03-10 23:12:44 -0300

    Set a loader to page home

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -6,16 +6,27 @@ import HomeBanner from './components/HomeBanner/HomeBanner';
 import PageBottom from '../../components/PageBottom/PageBottom';
 import HomeInfo from './components/HomeInfo/HomeInfo';
 import { Redirect } from 'react-router-dom'
+import Spinner from '../../components/Spinner/Spinner';
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => this.setState({ isLoading: false }), 3000)
+    }
 
     render() {
         if (this.props.email) return <Redirect to="/signup" />
-
+        if (this.state.isLoading) return <div className='container-spinner'><Spinner /></div>
         return (
             <div>
                 <section id='home-start' className='home-start'>
-                    <HomeHeader/>
+                    <HomeHeader />
                     <HomeBanner onClickSignup={this.props.onClickConfirmEmail} />
                 </section>
                 <section id='home-info' className='home-info'>
